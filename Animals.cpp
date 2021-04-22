3a0ad2fc2e8efa70be3a45927ddbfcd7b092850f
Slava Tiwari <62743855+SlavaSTiwari@users.noreply.github.com>
2021-04-22 17:15:30 +0530

    Задача на животных (ИСПРАВЛЕНО)
    Добавлено:
    --Ключевое слово "override" в нужных местах.
    --Аргументы по умолчанию для конструктора класса "Animal".
    
    Удалено:
    --Библиотека booster и теперь используется <string>

--- Animals.cpp
+++ Animals.cpp
@@ -1,30 +1,26 @@
-#include<bits/stdc++.h>
-
-using namespace std;
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<time.h>
 
 class Animal
 {
     private:
-        string name;
+        std::string name;
         int age;
         int weight;
 
     public:
         // Constructors:
-        Animal(){
-            name = "no name";
-            age = 0;
-            weight = 0;
-        }
-
-        Animal(string aName, int aAge, int aWeight){
+        Animal(std::string aName = "No name", int aAge = 0, int aWeight = 0){
             name = aName;
             age = aAge;
             weight = aWeight;
         }
 
         // Setters:
-        void set_name(string aName){
+        void set_name(std::string aName){
             name = aName;
         }
         void set_age(int aAge) {
@@ -35,7 +31,7 @@ class Animal
         }
 
         // Getters:
-        string get_name() {
+        std::string get_name() {
             return name;
         }
         int get_age() {
@@ -53,34 +49,34 @@ class Animal
 class Cat: public Animal
 {
     public:
-        void say(){
-            cout << "Mew!" << endl;
+         void say() override {
+            std::cout << "Mew!" << std::endl;
         }
 };
 
 class Dog: public Animal
 {
     public:
-        void say(){
-            cout << "Bork!" << endl;
+        void say() override{
+            std::cout << "Bork!" << std::endl;
         }
 };
 
 class Kangaroo: public Animal
 {
     public:
-        void say(){
-            cout << "Hello hooman!" << endl;
+        void say() override {
+            std::cout << "Hello hooman!" << std::endl;
         }
 };
 
 //Function prototypes:
-void generate_list(vector<Animal *> &vec, int c);
-void print_list(vector<Animal *> vec);
-void say_all(vector<Animal *> &vect);
-void five_fat_cats(vector<Animal *> &vect);
+void generate_list(std::vector<Animal *> &vect, int c);
+void print_list(std::vector<Animal *> vect);
+void say_all(std::vector<Animal *> &vect);
+void five_fat_cats(std::vector<Animal *> &vect);
 
-string random_name();
+std::string random_name();
 
 int main()
 {
@@ -96,7 +92,7 @@ int main()
     Dawko.say();
 
     // 2. Making generated list of random Animal(s):
-    vector<Animal *> a1;
+    std::vector<Animal *> a1;
 
     generate_list(a1, max_count);
     print_list(a1);
@@ -109,7 +105,7 @@ int main()
         return a->get_weight() < b->get_weight();
     });
 
-    cout << "\nThe fattest animal is: " << a1.back()->get_name() << " with weight a of: " << a1.back()->get_weight() << "Kgs" << endl;
+    std::cout << "\nThe fattest animal is: " << a1.back()->get_name() << " with weight a of: " << a1.back()->get_weight() << "Kgs" << std::endl;
 
     // 5. Finding the five fattest cats:
     five_fat_cats(a1);
@@ -117,49 +113,49 @@ int main()
     return 0;
 }
 
-void generate_list(vector<Animal *> &vec, int c){
+void generate_list(std::vector<Animal *> &vect, int c){
     int i;
     for(i=0;i<c;i++){
         int x = rand();
-        string ran_name = random_name();
+        std::string ran_name = random_name();
 
         if(x%3 == 0) {
             Animal *cat = new Cat;
             cat->set_name(ran_name + "-Cat");
             cat->set_age((rand()%(16-9+1))+9);
             cat->set_weight((rand()%(12-7+1))+7);
-            vec.push_back(cat);
+            vect.push_back(cat);
         }
         if(x%3 == 1) {
             Animal *dog = new Dog;
             dog->set_name(ran_name + "-Dog");
             dog->set_age((rand()%(13-10+1))+10);
             dog->set_weight((rand()%(45-21+1))+21);
-            vec.push_back(dog);
+            vect.push_back(dog);
         }
         if(x%3 == 2) {
             Animal *kang = new Kangaroo;
             kang->set_name(ran_name + "-Kangaroo");
             kang->set_age((rand()%(20-14+1))+14);
             kang->set_weight((rand()%(58-39+1))+39);
-            vec.push_back(kang);
+            vect.push_back(kang);
         }
     }
 }
 
-void print_list(vector<Animal *> vec) {
+void print_list(std::vector<Animal *> vect) {
 
-    for(int i=0; i<vec.size(); i++){
-        cout << vec[i]->get_name() << endl;
-        cout << vec[i]->get_age() << endl;
-        cout << vec[i]->get_weight() << "\n" << endl;
+    for(int i=0; i<vect.size(); i++){
+        std::cout << vect[i]->get_name() << std::endl;
+        std::cout << vect[i]->get_age() << " years" << std::endl;
+        std::cout << vect[i]->get_weight() << " Kgs" << "\n" << std::endl;
     }
 
 }
 
-void say_all(vector<Animal *> &vect) {
+void say_all(std::vector<Animal *> &vect) {
 
-    cout << "The animals say:" << endl;
+    std::cout << "The animals say:" << std::endl;
 
     for(int i=0; i<vect.size(); i++) {
         vect[i]->say();
@@ -167,24 +163,25 @@ void say_all(vector<Animal *> &vect) {
 
 }
 
-void five_fat_cats(vector<Animal *> &vect) {
+void five_fat_cats(std::vector<Animal *> &vect) {
 
     int c = 0;
 
-    cout << "\nThe 5 fattest cats are:" << endl;
+    std::cout << "\nThe 5 fattest cats are:" << std::endl;
     for(int i=vect.size(); i>0; i--) {
+
+        // If the sub-class is a cat, print the last 5 cats in descending order as the the array is already sorted:
         if(dynamic_cast<Cat *>(vect[i]) != nullptr && c < 5) {
-            cout << vect[i]->get_name() << " with a weight of: " << vect[i]->get_weight() << endl;
+            std::cout << vect[i]->get_name() << " with a weight of: " << vect[i]->get_weight() <<  "Kgs" << std::endl;
             c++;
         }
     }
-
 }
 
-string random_name(){
+std::string random_name(){
 
     int c;
-    string name("");
+    std::string name("");
     char cons[] = "bcdfghjklmnpqrstwvxyz";
     char vow[] = "aeiou";
 
